af21d88d90df177ac655d93804599d98fc8d504b
Ajarek <ajarek@poczta.onet.pl>
2023-09-06 00:47:56 +0200

    added globals.css style

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
+import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Test Next App',
@@ -10,8 +11,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={rubik.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
 export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ipsa corporis dolorum adipisci aut omnis voluptatum aperiam recusandae dolor eligendi ad facilis est, eos ab maiores assumenda, veniam in consequuntur placeat fugiat itaque. Odit delectus nulla accusamus qui sit error aut quibusdam consequatur quidem, vitae consequuntur voluptatem commodi voluptas minus!</p>
+      <div className="flex justify-center my-8" >
+       <Link href='/tickets'>
+        <button className='btn-primary'>View Tickets</button>
+       </Link>
+      </div>
     </main>
   )
 }

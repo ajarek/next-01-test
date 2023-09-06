043e2f06fbb3ba0350c1d82c13c5e8d8b1852758
Ajarek <ajarek@poczta.onet.pl>
2023-09-07 00:27:32 +0200

    added TicketList

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
+import Logo from './my-logo.png'
 export default function Navbar() {
   return (
     <nav>
+       <Image
+       src={Logo}
+       alt='Logo'
+       width={30}
+       quality={100}
+       placeholder='blur'
+       />
+      <h1>HelpDesk</h1>
       <Link href='/'>Dashboard</Link>
       <Link href='/tickets'>Tickets</Link>
     </nav>
